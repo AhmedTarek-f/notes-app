ec8f4e1746a767e43e0a5763142c956d261a6e4e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-27 06:51:11 +0300

    adding note model

---
+++ lib/features/notes_home/model/note_model.dart
@@ -0,0 +1,10 @@
+class NoteModel {
+  const NoteModel({
+    required this.noteTitle,
+    required this.noteBody,
+    required this.date
+  });
+  final String noteTitle;
+  final String noteBody;
+  final String date;
+}
