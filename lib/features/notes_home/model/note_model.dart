f061f7e595cf71d413ef41f5cf0e5fee18745a92
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-27 16:02:00 +0300

    making the note model accept to change of its title and content

--- lib/features/notes_home/model/note_model.dart
+++ lib/features/notes_home/model/note_model.dart
@@ -1,10 +1,28 @@
 class NoteModel {
-  const NoteModel({
+   NoteModel({
     required this.noteTitle,
     required this.noteBody,
     required this.date
   });
-  final String noteTitle;
-  final String noteBody;
+  String noteTitle;
+  String? noteBody;
   final String date;
+
+  // Convert NoteModel to Map
+  Map<String, dynamic> toJson() {
+    return {
+      'noteTitle': noteTitle,
+      'noteBody': noteBody,
+      'date': date,
+    };
+  }
+
+  // Convert Map to NoteModel
+  factory NoteModel.fromJson(Map<String, dynamic> json) {
+    return NoteModel(
+      noteTitle: json['noteTitle'],
+      noteBody: json['noteBody'],
+      date: json['date'],
+    );
+  }
 }
