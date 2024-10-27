61b433407d6082306a4de6b5b7e44d7f0742a766
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-27 06:48:45 +0300

    adding app styles

---
+++ lib/core/theme/constants/app_styles.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles
+{
+  static TextStyle stylesRegular14 =const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesMedium14 =const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesSemiBold14 =const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesBold14 =const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesRegular18 =const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesMedium18 =const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesSemiBold18 =const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesBold18 =const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesRegular16 =const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesMedium16 =const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesSemiBold16 =const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesBold16 =const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesRegular20 =const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesMedium20 =const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesSemiBold20 =const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesBold20 =const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesRegular24 =const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesMedium24 =const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesSemiBold24 =const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesBold24 =const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesRegular28 =const TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesMedium28 =const TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesSemiBold28 =const TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+    overflow: TextOverflow.ellipsis
+  );
+  static TextStyle stylesBold28 =const TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    overflow: TextOverflow.ellipsis
+  );
+}
